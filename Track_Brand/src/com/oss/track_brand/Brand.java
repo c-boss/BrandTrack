5418be23100cd9570a981c802895be1fd9c25c75
Dipika Jain <dipika_87@yahoo.com>
2013-07-16 23:05:41 -0700

    UI design

---
+++ Track_Brand/src/com/oss/track_brand/Brand.java
@@ -0,0 +1,16 @@
+package com.oss.track_brand;
+//store information about brand
+
+public class Brand {
+	private int iconID;
+	
+	public Brand(int iconID){
+		super();
+		this.iconID=iconID;
+	
+	}
+public int geticonID(){
+	return iconID;
+	
+}
+}
